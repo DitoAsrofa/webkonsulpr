bd70cd2e9864f654e30495e69baf8fdb449ecf87
DitoAsrofa <ditoasrofa212@gmail.com>
2024-10-18 16:45:03 +0700

    Fix redirect loop issue in index.php

--- api/index.php
+++ api/index.php
@@ -8,17 +8,21 @@ define('BASE_PATH', __DIR__);
 if (isset($_GET['reset'])) {
     session_unset(); // Hapus semua variabel sesi
     session_destroy(); // Hapus sesi saat ini
-    header("Location: ../api/login.php");
+    header("Location: login.php");
     exit();
 }
 
 // Cek apakah user sudah login
 if (!isset($_SESSION['user_id'])) {
     // Jika belum login, arahkan ke halaman landing page
-    header("Location: ../api/landing.php");
-    exit();
+    if (basename($_SERVER['PHP_SELF']) !== 'landing.php') {
+        header("Location: landing.php");
+        exit();
+    }
 } else {
     // Jika sudah login, arahkan ke halaman dashboard
-    header("Location: ../api/dashboard.php");
-    exit();
+    if (basename($_SERVER['PHP_SELF']) !== 'dashboard.php') {
+        header("Location: dashboard.php");
+        exit();
+    }
 }
