b043646d033e9189dca80ab5cc941f6bb8baab2a
DitoAsrofa <ditoasrofa212@gmail.com>
2024-10-18 16:36:39 +0700

    Update index.php untuk memperbaiki routing

--- api/index.php
+++ api/index.php
@@ -8,17 +8,17 @@ define('BASE_PATH', __DIR__);
 if (isset($_GET['reset'])) {
     session_unset(); // Hapus semua variabel sesi
     session_destroy(); // Hapus sesi saat ini
-    header("Location: views/login.php");
+    header("Location: ../api/login.php");
     exit();
 }
 
 // Cek apakah user sudah login
 if (!isset($_SESSION['user_id'])) {
     // Jika belum login, arahkan ke halaman landing page
-    header("Location: views/landing.php");
+    header("Location: ../api/landing.php");
     exit();
 } else {
     // Jika sudah login, arahkan ke halaman dashboard
-    header("Location: views/dashboard.php");
+    header("Location: ../api/dashboard.php");
     exit();
 }
